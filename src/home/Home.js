2bea3ca144b217d1d4d41d45c2fb46d4c80ac9f2
Kobikan <kobikan.linganathan@gmail.com>
2020-10-01 23:07:12 -0400

    Initial Grid of ships

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { useQuery, gql } from '@apollo/client';
 import './Home.css';
-
+import logos from "../assets/list.json";
 function Home() {
 
     const starships = gql`
@@ -17,16 +17,26 @@ function Home() {
     const {loading, error, data} = useQuery(starships);
     if(loading) return (<div/>)
     if(error) return (<div/>)
-    console.log(data)
+    console.log(logos.CR90corvette)
     return (
-      <div className="App">
-        <div class="card col-sm-3">
-          <img src="..." class="card-img-top"/>
-          <div class="card-body">
-            <p class="card-text"></p>
-          </div>
-        </div>
+      <div className="home">
+        <div className="container">
+        { data.allStarships.starships.map((starship,i) => {{
+          const key = logos[starship.name] ? logos[starship.name] : "unknown";
+          const temp = "3.png"
+          return(
+            <div class="card_layout card col-sm-3">
+              <img src={require(`../assets/${temp}`)} alt="Card image cap " class="card-img-top"/>
+              <div class="card-body">
+                <p class="card-text">{starship.name}</p>
+              </div>
+            </div>
+          )
+        }
+        })
+      }
       </div>
+    </div>
   );
 }
 
