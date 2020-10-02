8114f35b76cff26de469bf534ef1b35bc091558c
Kobikan <kobikan.linganathan@gmail.com>
2020-10-02 15:38:34 -0400

    Touch ups

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import history from '../services/history';
 import './Home.css';
 import NavBar from '../components/navbar';
 import logos from '../assets/list.json';
 function Home() {
-    const starships = gql`
+	const starships = gql`
     {
       allStarships{
         starships{
@@ -16,33 +16,35 @@ function Home() {
     }
     `;
 
-    const {loading, error, data} = useQuery(starships);
-    if(loading) return (<div/>);
-    if(error) return (<div/>);
-    console.log(data);
-    return (
-        <div className="home">
-            <NavBar/>
-            <div className="layout">
-                { data.allStarships.starships.map((starship,i) => {{
-                    const key = logos[starship.name] ? logos[starship.name] : 'unknown';
-                    const temp = '3.png';
-                    return(
-                        <div className="card-layout card" onClick = {() => {
-                            history.push(`/${starship.id}`);
-                        }}>
-                            <img src={require(`../assets/${temp}`)} alt="Card image cap " className="card-img-top"/>
-                            <div className="card-body">
-                                <p className="card-text">{starship.name}</p>
-                            </div>
-                        </div>
-                    );
-                }
-                })
-                }
-            </div>
-        </div>
-    );
+	const {loading, error, data} = useQuery(starships);
+	if(loading) return (<div/>);
+	if(error) return (<div/>);
+	console.log(data);
+	return (
+		<div className="home">
+			<NavBar/>
+			<div className="layout">
+				<div className="card-columns">
+
+					{ data.allStarships.starships.map((starship) => {{
+						const key = logos[starship.name] ? logos[starship.name] : 'unknown';
+						return(
+							<div className="card-layout card" onClick = {() => {
+								history.push(`/${starship.id}`);
+							}}>
+								<img src={require(`../assets/${key}`)} className="card-img-top"/>
+								<div className="card-body">
+									<p className="card-text">{starship.name}</p>
+								</div>
+							</div>
+						);
+					}
+					})
+					}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 
